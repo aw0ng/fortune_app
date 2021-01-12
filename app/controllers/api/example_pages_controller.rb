eb504e65c505daf82cfc31729777279abc5143c8
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-11 19:45:14 -0500

    lotto using while loop

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -8,19 +8,11 @@ class Api::ExamplePagesController < ApplicationController
 
   def lotto_method
     lotto = []
-    num1 = rand(1..60)
-    lotto << num1
-    num2 = rand(1..60)
-    lotto << num2
-    num3 = rand(1..60)
-    lotto << num3
-    num4 = rand(1..60)
-    lotto << num4
-    num5 = rand(1..60)
-    lotto << num5
-    num6 = rand(1..60)
-    lotto << num6
-    @lottery = lotto
+    index = 0
+    while index < 6
+      @lotto << rand(1..60)
+      index + = 1 
+    end
     render "lotto.json.jb"
   end
 end
